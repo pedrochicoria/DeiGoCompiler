d82db84c31a2c5d8a9291d93b5f9ad98aa830e5f
João Mendes <joaomendes@Joaos-MacBook-Pro-3.local>
2019-05-16 16:15:28 +0100

    asd2

--- Meta 4/testesC/exec.s
+++
@@ -1,6 +0,0 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.macosx_version_min 10, 14
-	.comm	_global.var.a,4,2       ## @global.var.a
-	.comm	_global.var.main,4,2    ## @global.var.main
-
-.subsections_via_symbols
